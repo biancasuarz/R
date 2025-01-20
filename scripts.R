eedc154bc0092b2231d73e01cb0714bd75486f95
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2025-01-20 19:38:05 -0300

    Exercices DataCamp 2

--- scripts.R
+++ scripts.R
@@ -30,3 +30,24 @@ names(roulette_vector) <- days_vector
 # Define a new variable based on a selection
 poker_midweek <- poker_vector[c(2,3,4)]
 poker_midweek
+
+# Define a new variable based on a selection
+roulette_selection_vector <- roulette_vector[2:5]
+roulette_selection_vector
+
+# Select poker results for Monday, Tuesday and Wednesday
+poker_start <- poker_vector[c("Monday","Tuesday","Wednesday")]
+poker_start
+  
+# Calculate the average of the elements in poker_start
+mean(poker_start)
+
+# Which days did you make money on poker?
+selection_vector <- poker_vector > 0
+  
+# Print out selection_vector
+selection_vector
+
+# Select from poker_vector these days
+poker_winning_days <- poker_vector[selection_vector]
+poker_winning_days
