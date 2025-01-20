5bc3a95924b005cff33d5c2d2b1fd4c9956191f3
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2025-01-20 19:23:48 -0300

    Exercices DataCamp

--- scripts.R
+++ scripts.R
@@ -19,3 +19,14 @@ total_week <- sum(poker_vector + roulette_vector)
 
 # Print out total_week
   total_week
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Define a new variable based on a selection
+poker_midweek <- poker_vector[c(2,3,4)]
+poker_midweek
